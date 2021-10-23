4cbeff64496d2d61c75f72464192b6e1727d4644
Judech <judefuanyi@gmail.com>
2021-10-23 06:47:54 -0700

    first commit

---
+++ python101.py
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Calculation of Area of Circle
+PI = 3.142
+raduis = int(input("Enter your raduis: "))
+print("Your raduis is "+ str(raduis))
+if(raduis, int):
+    Area = PI*raduis*raduis
+    print("Your Area is " + str(Area))
+else:
+    print("That number is not a number oh")
